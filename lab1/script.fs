ea4fa7ee4c4a126430ec8774404d0f802cac5cce
Щерба Артём Сергеевич <ip711s27@awp02.csc.local>
2019-09-03 13:19:25 +0700

    lab1: 50%

---
+++ lab1/script.fs
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo " Дата: " `date`
+echo " Имя учётной записи: " `echo $USER`
+echo " Доменное имя ПК: "`hostname`
+
+echo " Процессор: "
+TMP=`lscpu | grep "Имя модели"`
+printf "%7s  " •
+echo ${TMP/"Имя модели:"/"Модель - "}
+
+TMP=`lscpu | grep "Архитектура"`
+printf "%7s  " •
+echo ${TMP/"Архитектура:"/"Архитектура - "}
+
+TMP=`lscpu | grep -m 1 "CPU max MHz"`
+printf "%7s  " •
+echo ${TMP/"CPU max MHz:"/"Тактовая частота - "}
+
+TMP=`lscpu | grep "CPU(s):"`
+printf "%7s  " •
+echo ${TMP/"CPU(s):"/"Количество ядер - "}
+
+TMP=`lscpu | grep "Thread(s) per core:"`
+printf "%7s  " •
+echo ${TMP/"Thread(s) per core:"/"Количество потоков на одно ядро - "}
+
+echo " Оперативная память: "
+read -r _ TOTAL _ FREE _ <<< `free -h | grep "Mem:"`
+printf "%7s  " •
+echo "Всего - " $TOTAL
+
+TMP=`lscpu | grep "Архитектура"`
+printf "%7s  " •
+echo "Свободно - " $FREE
